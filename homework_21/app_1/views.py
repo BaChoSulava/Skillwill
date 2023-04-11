53098c135509bf8db54255815f1eac723164c9df
BaCho <bachana.sulava@gmail.com>
2023-04-11 18:47:49 +0400

    done

---
+++ homework_21/app_1/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Book
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'book_list.html'
+    context_object_name = 'books'
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'book_detail.html'
+    context_object_name = 'book'
+
+class BookCreateView(CreateView):
+    model = Book
+    template_name = 'book_form.html'
+    fields = ['title', 'author', 'published_date']
+    success_url = reverse_lazy('book_list')
+
+class BookUpdateView(UpdateView):
+    model = Book
+    template_name = 'book_form.html'
+    fields = ['title', 'author', 'published_date']
+    success_url = reverse_lazy('book_list')
+
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = 'book_confirm_delete.html'
+    success_url = reverse_lazy('book_list')
--- homework_21/homework_21/urls.py
+++ homework_21/homework_21/urls.py
@@ -1,21 +1,10 @@
-"""homework_21 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', BookListView.as_view(), name='book_list'),
+    path('<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('create/', BookCreateView.as_view(), name='book_create'),
+    path('<int:pk>/update/', BookUpdateView.as_view(), name='book_update'),
+    path('<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete'),
 ]
